ee1eec15685292285250ba8f715da21b600e6e3c
hdmanoach <hdmanoach@gmail.com>
2026-08-05 08:07:29 +0100

    Updated the .env file to include the DATABASE_URL and CORS_ORIGINS variables.

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 
 from database import Base, engine
 from middleware import LoggingMiddleware
 from routers import auth, logs, recipes
 
+# Charger les variables du fichier .env
+load_dotenv()
+
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+# Récupérer la chaîne du .env et la transformer en liste Python
+cors_origins_raw = os.getenv(
+    "CORS_ORIGINS", 
+    "http://localhost:3000,https://recette-frontend-five.vercel.app"
+)
+origins = [origin.strip() for origin in cors_origins_raw.split(",")]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=origins,  # Utilise la liste dynamique issue du .env
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
